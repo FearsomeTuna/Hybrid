05eb2311a6324a1eee35dde7aaa1c048362da264
GMondaca <gonzalo.mondaca.w@gmail.com>
2021-04-27 02:20:47 -0400

    Change train launch script.
    Pytorch version does not need be 1.4.0, so removed comment. Version
    info mention in readme file. Also,  current workflow does not use
    test.py yet, so changed script to reflect that.

--- tool/train.sh
+++ tool/train.sh
@@ -7,7 +7,7 @@
 
 export PYTHONPATH=./
 eval "$(conda shell.bash hook)"
-conda activate pt140  # pytorch 1.4.0 env
+conda activate ptEnv  # pytorch env
 PYTHON=python
 
 dataset=$1
@@ -26,6 +26,6 @@ $PYTHON -u ${exp_dir}/train.py \
   --config=${config} \
   2>&1 | tee ${model_dir}/train-$now.log
 
-$PYTHON -u ${exp_dir}/test.py \
-  --config=${config} \
-  2>&1 | tee ${result_dir}/test-$now.log
+#$PYTHON -u ${exp_dir}/test.py \
+#  --config=${config} \
+#  2>&1 | tee ${result_dir}/test-$now.log
