70a92f2192a9f220198541800c753464b12aecd7
Gonzalo Mondaca <elbailedelmono@gmail.com>
2022-03-26 11:35:13 -0300

    Add new model and changes
    ResNet and NL blocks now follow preactivation design.
    ResNet stem also changes to accommodate preactivation scheme.
    Added new doublebranch model for SBIR.

--- model/hybrid.py
+++ model/hybrid.py
@@ -1,9 +1,8 @@
-import torch
 import torch.nn as nn
 from torch import Tensor
 from functools import partial
 
-from typing import Optional, List
+from typing import Optional, List, Tuple, OrderedDict
 
 from util.util import init_weights
 from . import san
@@ -11,19 +10,19 @@ from . import resnet
 from . import nl
 
 import torchvision.models.resnet as torchres
+from itertools import chain
+from collections.abc import Iterable
 
-#  some parts are adapted from torchvision.models.resnet ResNet._make_layer method implementation.
+# some parts follow torchvision.models.resnet ResNet._make_layer method implementation.
 
 def _resnetStem():
+    # stem for preactivation version of resnet, no bn->relu
     outplanes = 64
     conv1 = nn.Conv2d(3, outplanes, kernel_size=7, stride=2, padding=3,
                       bias=False)
-    bn1 = nn.BatchNorm2d(outplanes)
-    relu = nn.ReLU(inplace=True)
     maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
     init_weights(conv1)
-    init_weights(bn1)
-    return nn.Sequential(conv1, bn1, relu, maxpool)
+    return nn.Sequential(conv1, maxpool)
 
 def _sanStem():
     outplanes = 64
@@ -54,7 +53,7 @@ class MixedModel(nn.Module):
             num_classes (int): amount of classes for classification
             stem (str): either 'res' (7x7 conv, stride 2 -> bn -> relu -> 2x2 maxpool) or 'san' (single 1x1 convolution). Both output 64 channels.
             sa_type (Optional[int]): san operation, 0 for pairwise, 1 for patchwise           
-            added_nl_blocks (Optional[List[int]], optional): amount of non-local blocks to add per stage.
+            added_nl_blocks (Optional[List[int]]): amount of non-local blocks to add per stage.
             Must be equal or less than number of blocks of respective stage. Defaults to None.
         """
         super().__init__()
@@ -77,17 +76,54 @@ class MixedModel(nn.Module):
             if t == 'nl' and added_blocks > 0:
                 raise ValueError('Bad argument: Non-Local stage cannot have added non-local blocks.')
 
-        if stem == 'san':
+        if stem == 'san' or stem == 'nl':
             self.stem = _sanStem()
         elif stem == 'res':
             self.stem = _resnetStem()
         else:
             raise ValueError('Invalid stem argument.')
+        self.backbone = _make_backbone(range(len(layers)), layers, layer_types, widths, sa_type, added_nl_blocks, nl_mode, in_widths, len(layers)-1)
+            
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(widths[-1], num_classes)
+
+    
+    
+    def forward(self, x: Tensor, getFeatVec: bool = False) -> Tensor:
+        x = self.stem(x)
+        x = self.backbone(x)
+
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        if getFeatVec:
+            return x
+        x = self.fc(x)
+        return x
+    
+    def load_state_dict_no_head(self, state_dict: OrderedDict[str, Tensor]):
+        """Loads just backbone portion of state_dict.
+
+        Note: this method mutates input state_dict.
+        """
+        for old_key in state_dict.copy().keys():
+            searchStr = 'fc.'
+            if searchStr in old_key:
+                del state_dict[old_key]
+        incompatible = self.load_state_dict(state_dict, strict=False)
+        if incompatible.unexpected_keys:
+            raise ValueError("Unexpected keys: {}".format(incompatible.unexpected_keys))
+        for k in incompatible.missing_keys:
+            searchStr = 'fc.'
+            if searchStr not in k:
+                raise ValueError("Missing key: {}".format(k))
+
+
+def _make_backbone(layer_nums: Iterable[int], layers: list[int], layer_types: list[str], widths: list[int], sa_type: Optional[int], added_nl_blocks: Optional[list[int]], nl_mode: str, in_widths: list[int], last_layer_num: int) -> nn.Sequential:
         backbone = []
 
-        for i, stage_type, blocks, added_nl, _inplanes, _outplanes in zip(range(len(layers)), layer_types, layers, added_nl_blocks, in_widths, widths):
+        for i, stage_type, blocks, added_nl, _inplanes, _outplanes in zip(layer_nums, layer_types, layers, added_nl_blocks, in_widths, widths):
             layer = []
-            if stage_type == 'san':
+            if stage_type == 'san':                
                 layer.append(san.TransitionLayer(_inplanes, _outplanes))
                 kernel_size = 3 if i == 0 else 7
                 if added_nl > 0:
@@ -102,31 +138,15 @@ class MixedModel(nn.Module):
                     layer.append(ResNetLayer(blocks, _inplanes, _outplanes, stride=stride))
             elif stage_type == 'nl':
                 layer.append(san.TransitionLayer(_inplanes, _outplanes))
-                layer.append(nl.NLLayer2D(blocks, _outplanes, mode=nl_mode))
+                layer.append(nl.NLLayer(blocks, _outplanes, mode=nl_mode))
             else:
                 raise ValueError('stage_type argument includes "' + stage_type + '" invalid argument.')
 
-            if i == len(layers)-1 and layer_types[-1] == 'san' or layer_types[-1] == 'nl': # last layer is san or nl
+            if i == last_layer_num: # bn->relu at end of backbone
                 layer.extend([nn.BatchNorm2d(widths[-1]), nn.ReLU(inplace=True)])
                 init_weights(layer[-2])
-            backbone.append(nn.Sequential(*layer))        
-
-        self.backbone = nn.Sequential(*backbone)
-
-        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
-        self.fc = nn.Linear(widths[-1], num_classes)
-    
-    def forward(self, x: Tensor, getFeatVec: bool = False) -> Tensor:
-        x = self.stem(x)
-        x = self.backbone(x)
-
-        x = self.avgpool(x)
-        x = x.view(x.size(0), -1)
-        if getFeatVec:
-            return x
-        x = self.fc(x)
-        return x
-
+            backbone.append(nn.Sequential(*layer))
+        return nn.Sequential(*backbone)
 
 def _shortcut(inplanes: int, outplanes: int, stride: int) -> Optional[nn.Sequential]:
     shortcut = None
@@ -193,7 +213,7 @@ class ResNetNLLayer(nn.Sequential):
         assert nl_blocks > 0        
 
         resBlock = partial(resnet.Bottleneck, outplanes, outplanes)
-        nlBlock = partial(nl.NLBlockND, in_channels=outplanes, mode=nl_mode, dimension=2)
+        nlBlock = partial(nl.NLBlock, in_channels=outplanes, mode=nl_mode)
 
         layers = []
 
@@ -250,7 +270,7 @@ class SanNLLayer(nn.Sequential):
 
         sanBlock = partial(san.Bottleneck, sa_type, inplanes, inplanes //
                            16, inplanes // 4, inplanes, 8, kernel_size, stride)
-        nlBlock = partial(nl.NLBlockND, inplanes, mode=nl_mode, dimension=2)
+        nlBlock = partial(nl.NLBlock, inplanes, mode=nl_mode)
         layers = []
 
         if nl_blocks*2 <= san_blocks:
@@ -266,3 +286,167 @@ class SanNLLayer(nn.Sequential):
 
         assert len(layers) == san_blocks + nl_blocks
         super().__init__(*layers)
+
+class MixedBiModal(nn.Module):
+    def __init__(self, layers: List[int], layer_types: List[str], widths: List[int], num_classes: int, stem: str, share: int, sa_type: Optional[int] = None, added_nl_blocks: Optional[List[int]] = None):
+        """Initializes model for image retrieval, based on independent initial sketch and image branches, and a shared
+        final branch.
+
+        General design follows MixedModel module, but model head includes an intermediary Linear layer for outputting a feature vector.
+
+        Args:
+            layers (List[int]): amount of blocks per each stage (not considering nl blocks).
+            layer_types (List[str]): list indicating the type of block to use in every stage. Every element can be one of 'san', 'res' or 'nl'.
+            widths (List[int]): list indicating output channel size for each stage.
+            num_classes (int): amount of classes for classification
+            stem (str): either 'res' (7x7 conv, stride 2 -> bn -> relu -> 2x2 maxpool) or 'san' (single 1x1 convolution). Both output 64 channels.
+            share (int): amount of block stages that share weights. Cannot be greater than the total amount of stages (length of layers param). Stem is never shared. Model head is always shared. Must be larger than 0.
+            sa_type (Optional[int]): san operation, 0 for pairwise, 1 for patchwise           
+            added_nl_blocks (Optional[List[int]], optional): amount of non-local blocks to add per stage. Must be equal or less than number of blocks of respective stage. Defaults to None.
+        """
+        super().__init__()
+        assert share <= len(layers) and share > 0
+
+        featVecLen = 1024
+        self._stages = len(layers)
+        self._shareQty = share
+        self._unsharedQty = self._stages - self._shareQty
+        inplanes = 64
+        nl_mode = 'dot'
+        in_widths = [inplanes] + widths[:-1]
+        
+        assert len(layers) == len(layer_types)
+        assert len(layers) == len(widths)
+        if 'san' in layer_types:
+            assert sa_type == 0 or sa_type == 1
+        if added_nl_blocks == None:
+            added_nl_blocks = [0]*len(layers)        
+        assert len(added_nl_blocks) == len(layers)        
+        for t, added_blocks in zip(layer_types, added_nl_blocks):
+            if t == 'nl' and added_blocks > 0:
+                raise ValueError('Bad argument: Non-Local stage cannot have added non-local blocks.')
+        
+        if stem == 'san':
+            self.sketchStem = _sanStem()
+            self.imageStem = _sanStem()
+        elif stem == 'res':
+            self.sketchStem = _resnetStem()
+            self.imageStem = _resnetStem()
+        else:
+            raise ValueError('Invalid stem argument.')
+        
+        last_layer_num = len(layers)-1
+        self.sketchBackbone = _make_backbone(range(self._unsharedQty), layers[:self._unsharedQty], layer_types[:self._unsharedQty], widths[:self._unsharedQty], sa_type, added_nl_blocks[:self._unsharedQty], nl_mode, in_widths[:self._unsharedQty], last_layer_num)
+        self.imageBackbone = _make_backbone(range(self._unsharedQty), layers[:self._unsharedQty], layer_types[:self._unsharedQty], widths[:self._unsharedQty], sa_type, added_nl_blocks[:self._unsharedQty], nl_mode, in_widths[:self._unsharedQty], last_layer_num)
+        self.shareBackbone = _make_backbone(range(self._unsharedQty, len(layers)), layers[self._unsharedQty:], layer_types[self._unsharedQty:], widths[self._unsharedQty:], sa_type, added_nl_blocks[self._unsharedQty:], nl_mode, in_widths[self._unsharedQty:], last_layer_num)
+        
+        self.relu = nn.ReLU(inplace=True)
+        self.shareFc = nn.Linear(widths[-1], 1024)
+        self.bn = nn.BatchNorm1d(1024)
+
+        self.shareFeatVecFc = nn.Linear(1024, featVecLen)
+        self.classifier = nn.Linear(featVecLen, num_classes)
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+
+    
+    def forward(self, *args: Tensor, mode: Optional[str] = None) -> Tuple[List[Tensor], List[Tensor]]:
+        """Returns feature vector and model output.
+        Args:
+            *args: First argument will be processed using sketch+shared branch, all subsequent arguments will be
+              processed using image+shared branch. At least one argument must be passed.
+            mode (str): If not None, either 'sketch', 'image' to process only first input, and using the respective model branch.
+              Equivalent to using forward_sketch or forward_image. Defaults to None.
+
+
+        Returns:
+            Tuple with feature embedding list and classification outputs list, in the same relative order as in input arguments.
+            Eg. forward(sketch, image1, image2) will return [sketchFeat, imageFeat1, imageFeat2], [sketchOut, imageOut1, imageOut2]
+        """
+        assert len(args) > 0
+        if mode == 'sketch':
+            return self.forward_sketch(args[0])
+        if mode == 'image':
+            return self.forward_image(args[0])
+        
+        sketch = args[0]
+
+        sketchFeatVec, sketch = self.forward_sketch(sketch)
+
+        featVecs = []
+        outputs = []
+        for image in args[1:]:
+            imageFeatVec, image = self.forward_image(image)
+            featVecs.append(imageFeatVec)
+            outputs.append(image)
+        
+        featVecs.insert(0, sketchFeatVec)
+        outputs.insert(0, sketch)
+        return featVecs, outputs
+
+    def forward_sketch(self, sketch: Tensor) -> Tensor:
+        sketch = self.sketchStem(sketch)
+        sketch = self.sketchBackbone(sketch)
+        sketch = self.shareBackbone(sketch)
+        sketch = self.avgpool(sketch)
+        sketch = sketch.view(sketch.size(0), -1)
+        sketch = self.relu(self.bn(self.shareFc(sketch)))
+        sketchFeatVec = self.shareFeatVecFc(sketch)
+        sketch = self.classifier(sketchFeatVec)
+        return sketchFeatVec, sketch
+
+    def forward_image(self, image: Tensor) -> Tensor:
+        image = self.imageStem(image)
+        image = self.imageBackbone(image)
+        image = self.shareBackbone(image)
+        image = self.avgpool(image)
+        image = image.view(image.size(0), -1)
+        image = self.relu(self.bn(self.shareFc(image)))
+        imageFeatVec = self.shareFeatVecFc(image)
+        image = self.classifier(imageFeatVec)
+        return imageFeatVec, image
+    
+    def load_unshared_state_dict(self, sketch_state_dict: OrderedDict[str, Tensor], image_state_dict: OrderedDict[str, Tensor]) -> None:
+        """Loads unshared layers state from pretrained MixedModel modules for sketch and image portion
+        of the model.
+
+        Note: this method mutates input state_dicts.
+        """
+        assert len(sketch_state_dict.keys())==len(image_state_dict.keys())
+        # filter out non-shared parts of backbone
+        for old_sketch_key, old_image_key in zip(sketch_state_dict.copy().keys(), image_state_dict.copy().keys()):
+            for i in range(self._unsharedQty, self._stages):
+                searchStr = 'backbone.{}'.format(i)
+                if searchStr in old_sketch_key:
+                    del sketch_state_dict[old_sketch_key]
+                if searchStr in old_image_key:
+                    del image_state_dict[old_image_key]
+                    
+        # take and rename only keys related to backbone and stem
+        new_dict = dict()
+        for old_sketch_key, old_image_key in zip(sketch_state_dict.keys(), image_state_dict.keys()):
+            if 'backbone' in old_sketch_key or 'stem' in old_sketch_key:
+                new_sketch_key = old_sketch_key.replace('stem', 'sketchStem')
+                new_sketch_key = new_sketch_key.replace('backbone', 'sketchBackbone')
+                new_dict[new_sketch_key] = sketch_state_dict[old_sketch_key]
+            if 'backbone' in old_image_key or 'stem' in old_image_key:
+                new_image_key = old_image_key.replace('stem', 'imageStem')
+                new_image_key = new_image_key.replace('backbone', 'imageBackbone')
+                new_dict[new_image_key] = image_state_dict[old_image_key]
+        
+        incompatible = self.load_state_dict(new_dict, strict=False)
+        if incompatible.unexpected_keys:
+            raise ValueError("Unexpected keys for non shared portion of model: {}".format(incompatible.unexpected_keys))
+        for k in incompatible.missing_keys:
+            for i in range(self._unsharedQty):
+                searchStrSketch = 'sketchBackbone'
+                searchStrImage = 'imageBackbone'
+                searchStrStem = 'Stem'
+                if searchStrSketch in k or searchStrImage in k or searchStrStem in k:
+                    raise ValueError("Missing key for non shared portion of model: {}".format(k))
+        
+    
+    def unsharedParameters(self):
+        return chain(self.sketchStem.parameters(), self.sketchBackbone.parameters(), self.imageStem.parameters(), self.imageBackbone.parameters())
+    
+    def sharedParameters(self):
+        return chain(self.shareBackbone.parameters(), self.shareFeatVecFc.parameters(), self.shareFc.parameters(), self.bn.parameters(), self.classifier.parameters())
--- model/resnet.py
+++ model/resnet.py
@@ -1,49 +1,60 @@
-import torch
 from torch import Tensor
 import torch.nn as nn
-from typing import Type, Any, Callable, Union, List, Optional
+from typing import Callable, Optional
 
-import torchvision.models.resnet as torchres
+from torchvision.models.resnet import conv1x1, conv3x3
 from util.util import init_weights
 
-class ResNet(torchres.ResNet):
-    def __init__(self, layers: List[int], num_classes: int = 1000) -> None:
-        super().__init__(torchres.Bottleneck, layers, num_classes=num_classes)
-    
-    def forward(self, x: Tensor, getFeatVec: bool=False) -> Tensor:
-        x = self.conv1(x)
-        x = self.bn1(x)
-        x = self.relu(x)
-        x = self.maxpool(x)
+class Bottleneck(nn.Module):
+    expansion = 4
 
-        x = self.layer1(x)
-        x = self.layer2(x)
-        x = self.layer3(x)
-        x = self.layer4(x)
+    def __init__(self, inplanes: int, outplanes: int, stride: int = 1, downsample: Optional[nn.Module] = None,
+                 groups: int = 1, base_width: int = 64, dilation: int = 1, norm_layer: Optional[Callable[..., nn.Module]] = None) -> None:
+        """Initializes a resnet bottleneck block with full preactivation. Based on torchvision postactivation implementation.
 
-        x = self.avgpool(x)
-        x = torch.flatten(x, 1)
-        if getFeatVec:
-            return x
-        x = self.fc(x)
-        return x
+        This implementation places the stride for downsampling at 3x3 convolution(self.c
+        while original implementation places the stride at the first 1x1 convolution(sel
+        according to "Deep residual learning for image recognition"https://arxiv.org/abs
+        This is based off of ResNet V1.5 (postactivation) and improves accuracy accordin
+        https://ngc.nvidia.com/catalog/model-scripts/nvidia:resnet_50_v1_5_for_pytorch.
 
-class Bottleneck(torchres.Bottleneck):
-    # Bottleneck in torchvision places the stride for downsampling at 3x3 convolution(self.conv2)
-    # while original implementation places the stride at the first 1x1 convolution(self.conv1)
-    # according to "Deep residual learning for image recognition"https://arxiv.org/abs/1512.03385.
-    # This variant is also known as ResNet V1.5 and improves accuracy according to
-    # https://ngc.nvidia.com/catalog/model-scripts/nvidia:resnet_50_v1_5_for_pytorch.
-    
-    def __init__(self, inplanes: int, outplanes: int, stride: int = 1, downsample: Optional[nn.Module] = None, groups: int = 1, base_width: int = 64, dilation: int = 1, norm_layer: Optional[Callable[..., nn.Module]] = None) -> None:
-        # in torchvision implementation, Bottleneck 'planes' argument corresponds to output planes but is internally expanded by res.Bottleneck.expansion factor (equal to 4).
-        # Hence unintuitive instantiations like res.Bottleneck(256, 64), which is a block with 256 input planes and 256 output planes
-        # We abstract this for clarity when declaring this adapted version.
-        assert outplanes % torchres.Bottleneck.expansion == 0
-        planes = outplanes // torchres.Bottleneck.expansion
-        super().__init__(inplanes, planes, stride=stride, downsample=downsample, groups=groups, base_width=base_width, dilation=dilation, norm_layer=norm_layer)
+        Args:
+            inplanes (int): input channels.
+            outplanes (int): output channels. Must be divisible by self.expansion factor (4).
+            stride (int): stride for middle convolution. This dictates spatial resolution downsampling.
+            downsample (Optional[nn.Module]): module responsible for downsampling. Out dimensions must match with block output.
+            groups (int): groups for middle convolution
+            dilation (int): dilation for middle convolution
+            norm_layer (Optional[Callable[..., nn.Module]]): batch normalization layer to be used. Defaults to torch.nn.BatchNorm2d
+        """
+        super(Bottleneck, self).__init__()
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+        assert outplanes % self.expansion == 0
+        planes = outplanes // self.expansion
+        width = int(planes * (base_width / 64.)) * groups
+        # Both self.conv2 and self.downsample layers downsample the input when stride != 1
+        self.bn1 = norm_layer(inplanes)
+        self.conv1 = conv1x1(inplanes, width)
+        self.bn2 = norm_layer(width)
+        self.conv2 = conv3x3(width, width, stride, groups, dilation)
+        self.bn3 = norm_layer(width)
+        self.conv3 = conv1x1(width, planes * self.expansion)
+        self.relu = nn.ReLU(inplace=True)
+        self.downsample = downsample
+        self.stride = stride
         init_weights(self)
 
-def resnet(layers, num_classes):
-    model = ResNet(layers=layers, num_classes=num_classes)
-    return model
+    def forward(self, x: Tensor) -> Tensor:
+        identity = x
+
+        out = self.conv1(self.relu(self.bn1(x)))
+        out = self.conv2(self.relu(self.bn2(out)))
+        out = self.conv3(self.relu(self.bn3(out)))
+
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        out += identity
+
+        return out
